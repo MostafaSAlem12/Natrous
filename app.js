698c5c0f860a7196b4cc25e49a42ee9110a8b0d0
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2024-11-18 14:24:40 +0200

    Add Pug

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const path = require('path'); // Importing path module
+const dotenv = require('dotenv'); // For environment variables
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -12,15 +14,18 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
+dotenv.config(); // Load environment variables
+
 const app = express();
 
+// Set Pug as view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
 
-app.set('view engine', 'pug')
-app.set('views',path.join(__dirname),'./views')
 // 1) GLOBAL MIDDLEWARES
 
 // Serving static files
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
 app.use(helmet());
@@ -61,19 +66,16 @@ app.use(
   })
 );
 
-
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
   next();
 });
 
 // 3) ROUTES
-app.get('/' ,(req,res)=>{
-  res.status(200).render('base')
-})
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
