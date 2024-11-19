265363b7a432f712aee279d3b97cc9dc47ece55a
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2024-11-19 14:28:55 +0200

    use multer to upload images

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes')
 
 dotenv.config(); // Load environment variables
 
@@ -92,7 +93,7 @@ app.get('/tour', (req,res)=>{
   })
 })
 
-
+app.use('/', viewRouter)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
